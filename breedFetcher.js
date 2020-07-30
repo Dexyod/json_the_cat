200b995e1f43f0a31ce608bf4030792b91e9e6ab
Dexyod <dexyod43@gmail.com>
2020-07-30 17:00:31 +0000

    completed assignment

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,23 @@
 const request = require("request");
+
+const args = process.argv.slice(2);
+
+request(
+  `https://api.thecatapi.com/v1/breeds/search?name=${args[0]}`,
+  (err, response, body) => {
+    const data = JSON.parse(body);
+    if (err) {
+      console.log("Error: ", err);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      console.log(`StatusCode: ${response} ${response.statusCode}`);
+      return;
+    }
+    if (data[0] === undefined) {
+      console.log("Please enter valid breed");
+      return;
+    }
+    console.log(data[0].description);
+  }
+);
